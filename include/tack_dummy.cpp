e250c09ecd18d3c8371a3249d2f33ff423441344
aidan <aidan.dodds@googlemail.com>
2016-10-18 23:40:14 +0100

    Add test suite and cmake infrastructure

---
+++ include/tack_dummy.cpp
@@ -0,0 +1,3 @@
+// this file only exists to allow cmake to generate a valid library
+
+#include "tack/float4.h"
---
+++ testing/test_float4.cpp
@@ -0,0 +1,33 @@
+#include "testing.h"
+#include "../include/tack/float4.h"
+
+struct test_float4_t : public test_t {
+
+    test_float4_t()
+        : test_t("test_float4")
+    {
+        register_test(this);
+    }
+
+    void test1()
+    {
+        using namespace tack;
+
+        float4 a(0.1f);
+        float4 b(0.1f, 0.2f, 0.3f, 0.4f);
+        for (int i = 0; i < 10; ++i) {
+            a += b;
+        }
+        float4 c = b;
+    }
+
+    virtual bool run() override
+    {
+        test1();
+        return passed();
+    }
+};
+
+namespace {
+test_float4_t test_instance;
+} // namespace {}
