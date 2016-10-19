a107304ac7baae2cf17f9426c9f94a310ee2c4ae
aidan <aidan.dodds@googlemail.com>
2016-10-19 22:16:17 +0100

    add abs value function

--- testing/test_float4.cpp
+++ testing/test_float4.cpp
@@ -1,33 +1,27 @@
 #include "testing.h"
 #include <tack/float4.h>
+#include <array>
 
-struct test_float4_t : public test_t {
+struct test_float4_abs_t : public test_t {
 
-    test_float4_t()
-        : test_t("test_float4")
+    test_float4_abs_t()
+            : test_t("test_float4_abs")
     {
         register_test(this);
     }
 
-    void test1()
-    {
-        using namespace tack;
-
-        float4 a(0.1f);
-        float4 b(0.1f, 0.2f, 0.3f, 0.4f);
-        for (int i = 0; i < 10; ++i) {
-            a += b;
-        }
-        float4 c = b;
-    }
-
     virtual bool run() override
     {
-        test1();
+        using namespace tack;
+        const float4 a(-1, 2, -3, 4);
+        const float4 b = abs(a);
+        std::array<float, 4> out;
+        b.store(out.data());
+        pass_ &= out[0]==1.f && out[1]==2.f && out[2]==3.f && out[3]==4.f;
         return passed();
     }
 };
 
 namespace {
-test_float4_t test_instance;
+test_float4_abs_t test_1;
 } // namespace {}
