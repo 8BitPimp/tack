ad0bc08301163cfc0a3f900a4ab8c4ee8f94dd56
Kenneth Benzie (Benie) <k.benzie83@gmail.com>
2016-10-19 22:28:32 +0100

    Add helper macros to simplify writing tests.
    * Add TEST(GROUP, NAME) macro to define test cases.
    * Add ASSERT(CONDITION) macro to end test on failure.
    * Add EXPECT(CONDITION) macro to mark as failure but continue.

--- testing/test_float4.cpp
+++ testing/test_float4.cpp
@@ -2,26 +2,23 @@
 #include <tack/float4.h>
 #include <array>
 
-struct test_float4_abs_t : public test_t {
+TEST(float4, abs)
+{
+    using namespace tack;
+    const float4 a(-1, 2, -3, 4);
+    const float4 b = abs(a);
+    std::array<float, 4> out;
+    b.store(out.data());
+    ASSERT(out[0] == 1.f && out[1] == 2.f && out[2] == 3.f && out[3] == 4.f);
+}
 
-    test_float4_abs_t()
-        : test_t("test_float4_abs")
-    {
-        register_test(this);
-    }
-
-    virtual bool run() override
-    {
-        using namespace tack;
-        const float4 a(-1, 2, -3, 4);
-        const float4 b = abs(a);
-        std::array<float, 4> out;
-        b.store(out.data());
-        pass_ &= out[0] == 1.f && out[1] == 2.f && out[2] == 3.f && out[3] == 4.f;
-        return passed();
-    }
-};
-
-namespace {
-test_float4_abs_t test_1;
-} // namespace {}
+TEST(float4, equal)
+{
+    using namespace tack;
+    float4 a(42, 23, 48, 64);
+    float4 b = a;
+    float4 c = a == b;
+    std::array<float, 4> out;
+    c.store(out.data());
+    ASSERT(out[0] && out[1] && out[2] && out[3]);
+}
--- testing/testing.h
+++ testing/testing.h
@@ -9,7 +9,7 @@ struct test_t {
     {
     }
 
-    virtual bool run() = 0;
+    virtual void run() = 0;
 
     const char* name() const
     {
@@ -43,3 +43,32 @@ protected:
 };
 
 void register_test(test_t* instance);
+
+#define TEST(GROUP, NAME)                           \
+    namespace {                                     \
+        struct GROUP##_##NAME##_t : public test_t { \
+            GROUP##_##NAME##_t()                    \
+                : test_t(#GROUP "." #NAME)          \
+            {                                       \
+                register_test(this);                \
+            }                                       \
+            virtual void run();                     \
+        } GROUP##_##NAME;                           \
+    }                                               \
+    void GROUP##_##NAME##_t::run()
+
+#define ASSERT(CONDITION)        \
+    {                            \
+        if (!(CONDITION)) {      \
+            this->pass_ = false; \
+        } else {                 \
+            return;              \
+        }                        \
+    }
+
+#define EXPECT(CONDITION)        \
+    {                            \
+        if (!(CONDITION)) {      \
+            this->pass_ = false; \
+        }                        \
+    }
