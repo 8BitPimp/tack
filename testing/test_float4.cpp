2949af8e2318b4b35c46c20c977e6a43cf833e75
aidan <aidan.dodds@googlemail.com>
2016-10-20 09:26:48 +0100

    refactor to use new testing helpers

--- testing/test_float4.cpp
+++ testing/test_float4.cpp
@@ -1,6 +1,6 @@
 #include "testing.h"
-#include <tack/float4.h>
 #include <array>
+#include <tack/float4.h>
 
 TEST(float4, abs)
 {
@@ -22,6 +22,46 @@ TEST(float4, equal)
     c.store(out.data());
     ASSERT_MSG(out[0] && out[1] && out[2] && out[3],
         "c is not equal to a");
+}
+
+TEST(float4, extract)
+{
+    using namespace tack;
+    const float4 a(1.f, 2.f, 3.f, 4.f);
+    ASSERT(a.x() == 1.f);
+    ASSERT(a.y() == 2.f);
+    ASSERT(a.z() == 3.f);
+    ASSERT(a.w() == 4.f);
+}
+
+TEST(float4, blend)
+{
+    using namespace tack;
+    const float4 a(1.f);
+    const float4 b(2.f);
+    const float4 c = blend<e_aabb>(a, b);
+    ASSERT(c.x() == 1.f);
+    ASSERT(c.y() == 1.f);
+    ASSERT(c.z() == 2.f);
+    ASSERT(c.w() == 2.f);
+}
+
+TEST(float4, swizzle)
+{
+    using namespace tack;
+    std::array<float, 4> out;
+    {
+        const float4 a(1.f, 2.f, 3.f, 4.f);
+        const float4 b = a.swizzle<e_xzyw>();
+        b.store(out.data());
+        ASSERT((out[0] == 1.f) && (out[1] == 3.f) && (out[2] == 2.f) && (out[3] == 4.f));
+    }
+    {
+        const float4 a(1.f, 2.f, 3.f, 4.f);
+        const float4 b = a.swizzle<e_xxyy>();
+        b.store(out.data());
+        ASSERT((out[0] == 1.f) && (out[1] == 1.f) && (out[2] == 2.f) && (out[3] == 2.f));
+    }
 }
 
 TEST(float4, unary_minus)
