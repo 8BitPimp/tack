5f78ee358c3fbef6347f6db9747e21300943beda
Kenneth Benzie <k.benzie83@gmail.com>
2016-10-20 08:44:45 +0100

    Add helper macros to simplify writing tests. (#9)
    * Add helper macros to simplify writing tests.
    
    * Add TEST(GROUP, NAME) macro to define test cases.
    * Add ASSERT(CONDITION) macro to end test on failure.
    * Add EXPECT(CONDITION) macro to mark as failure but continue.
    
    * Add printf style message macro variants
    
    * Add ASSERT_MSG(CONDITION, ...) to print a message and return on failure.
    * Add EXPECT_MSG(CONDITION, ...) to print a message on failure.

--- testing/testing.h
+++ testing/testing.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 
 struct test_t {
 
@@ -9,7 +10,7 @@ struct test_t {
     {
     }
 
-    virtual bool run() = 0;
+    virtual void run() = 0;
 
     const char* name() const
     {
@@ -26,9 +27,24 @@ struct test_t {
         return !pass_;
     }
 
+    template <class... Args>
+    void format_message(Args... args)
+    {
+        std::vector<char> buffer(snprintf(nullptr, 0, args...) + 1);
+        snprintf(buffer.data(), buffer.size(), args...);
+        message_ += buffer.data();
+    }
+
+    void format_message(const char* format)
+    {
+        std::vector<char> buffer(strlen(format) + 1);
+        snprintf(buffer.data(), buffer.size(), "%s", format);
+        message_ += buffer.data();
+    }
+
     const char* message() const
     {
-        return message_.c_str();
+        return message_.data();
     }
 
     bool has_message() const
@@ -43,3 +59,52 @@ protected:
 };
 
 void register_test(test_t* instance);
+
+#define TEST(GROUP, NAME)                           \
+    namespace {                                     \
+        struct GROUP##_##NAME##_t : public test_t { \
+            GROUP##_##NAME##_t()                    \
+                : test_t(#GROUP "." #NAME)          \
+            {                                       \
+                register_test(this);                \
+            }                                       \
+            virtual void run();                     \
+        } GROUP##_##NAME;                           \
+    }                                               \
+    void GROUP##_##NAME##_t::run()
+
+#define ASSERT(CONDITION)                                        \
+    {                                                            \
+        if (!(CONDITION)) {                                      \
+            this->pass_ = false;                                 \
+            this->format_message("%s:%d: ", __FILE__, __LINE__); \
+            return;                                              \
+        }                                                        \
+    }
+
+#define ASSERT_MSG(CONDITION, ...)                               \
+    {                                                            \
+        if (!(CONDITION)) {                                      \
+            this->pass_ = false;                                 \
+            this->format_message("%s:%d: ", __FILE__, __LINE__); \
+            this->format_message(__VA_ARGS__);                   \
+            return;                                              \
+        }                                                        \
+    }
+
+#define EXPECT(CONDITION)                                        \
+    {                                                            \
+        if (!(CONDITION)) {                                      \
+            this->pass_ = false;                                 \
+            this->format_message("%s:%d: ", __FILE__, __LINE__); \
+        }                                                        \
+    }
+
+#define EXPECT_MSG(CONDITION, ...)                               \
+    {                                                            \
+        if (!(CONDITION)) {                                      \
+            this->pass_ = false;                                 \
+            this->format_message("%s:%d: ", __FILE__, __LINE__); \
+            this->format_message(__VA_ARGS__);                   \
+        }                                                        \
+    }
